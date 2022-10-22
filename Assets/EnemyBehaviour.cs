73aaf69f39382b48c9dcb4e9409b5f98c5f4b77b
David Espinoza <70979506+davis170559@users.noreply.github.com>
2022-10-22 14:51:04 -0500

    Cambios David
    Le añadi sonido a las balas y los ovnis y tambien corregi la posicion de los ovnis y del prededaor. Se pueden eliminar enemigos ahora.

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -10,6 +10,7 @@ public class EnemyBehaviour : MonoBehaviour
     public float damage = 10.0f;
     public float lifeTime = 30.0f;
     public int rarity = 1;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +30,17 @@ public class EnemyBehaviour : MonoBehaviour
     {
         yield return new WaitForSeconds(lifeTime);
         Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("Collision");
+        if(other.CompareTag("Playerbullet")){
+            health -= 15;
+            
+        }
+        if(health <=0 ){
+            AudioManager.Instance.PlayDestruction();
+            Destroy(this.gameObject);
+        }
     }
 }
