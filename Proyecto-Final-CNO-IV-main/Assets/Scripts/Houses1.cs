1c9d37a3f5160172e5b31d19e5626f61921255ed
Jose Guadalupe Yañez Hernandez <99089281+JoseYanez047@users.noreply.github.com>
2022-11-25 21:56:46 -0600

    MenuInicio
    Se agregaron los menus de inicio asi como los las opciones y la musica de inicio

---
+++ Proyecto-Final-CNO-IV-main/Assets/Scripts/Houses1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Houses1 : MonoBehaviour
+{
+    private float backGroundRepeatWidth=29.3f;
+    
+    void Start()
+    {
+        //get box collider 2d component and get the size of the box collider
+
+
+        BoxCollider2D groundCollider = GetComponent<BoxCollider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the background to the left
+        transform.Translate(Vector3.left * Time.deltaTime * 4);
+        //if the background is off the screen, move it to the right
+        if (transform.position.x < -backGroundRepeatWidth)
+        {
+            transform.position = new Vector3(backGroundRepeatWidth, 0.92f, 0);
+        }
+        
+        
+    }
+}
---
+++ Proyecto-Final-CNO-IV-main/README.md
@@ -0,0 +1,2 @@
+# Runner
+ Proyecto Final
