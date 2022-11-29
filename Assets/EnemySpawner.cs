0c9a7da52c2911f480633b47026281233cdb2ef9
ThorgyOrb <112546683+ThorgyOrb@users.noreply.github.com>
2022-11-28 18:26:39 -0600

    Revert "Revert "Cambios de proyecto Avance -EmanuelRdz""

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject[] enemyPrefabs;
+    public GameObject coinPrefab;
     public float minSpawnRate = 2.5f;
     public float maxSpawnRate = 6.0f;
+    private PlayerControler playerControllerScript;
+    public float spawnDelay = 5;
+    public float spawnInterval = 4.2f;
     // Start is called before the first frame update
     void Start()
     {
         //Execute the coroutine
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControler>();
         StartCoroutine(SpawnEnemies());
+        InvokeRepeating("SpawnObjects", spawnDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -21,9 +27,8 @@ public class EnemySpawner : MonoBehaviour
     }
 
     //Create coroutine to spawn enemies every 5 seconds
-    IEnumerator SpawnEnemies()
-    {
-        while (true)
+    IEnumerator SpawnEnemies(){
+        while (!playerControllerScript.gameOver)
         {
             yield return new WaitForSeconds(Random.Range(minSpawnRate, maxSpawnRate));
 
@@ -67,5 +72,19 @@ public class EnemySpawner : MonoBehaviour
                 }
             }
         }
+    }
+    // Spawn Coins and enemies
+    void SpawnObjects (){
+        // Set random spawn location and random object index
+        Vector3 spawnLocation = new Vector3(15, Random.Range(1.5f, -5), 0);
+        //Vector3 spawnPosition = new Vector3(this.transform.position.x + Random.Range(-2, 2), this.transform.position.y + Random.Range(-3, 6), this.transform.position.z);
+        //int index = Random.Range(0, objectPrefabs.Length);
+
+        // If game is still active, spawn new object
+        if (!playerControllerScript.gameOver)
+        {
+            Instantiate(coinPrefab, spawnLocation, Quaternion.identity);
+        }
+
     }
 }
